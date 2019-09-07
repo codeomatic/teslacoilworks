da1129b581e920e81e1a11817235c714288fa13c
Denis <fsdvn1@gmail.com>
2019-09-06 18:40:06 -0700

    Reach preview meta was added.

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,8 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Don Anderson'
 SITENAME = 'Tesla Coil Works'
-SITEURL = ''
+SITEURL = 'https://www.teslacoilworks.com'
+DESCRIPTION = 'The most powerful Tesla coils in the Pacific Northwest!'
 
 PATH = 'content'
 
